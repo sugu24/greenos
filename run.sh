e2025584bce16d10d8f48e5fd4a1eb52830af05b
sugu24 <shayasugu@icloud.com>
2023-09-07 13:27:12 +0900

    boot

--- run.sh
+++ run.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 set -xue
 
-qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio
+QEMU=qemu-system-riscv32
+
+# clangのパス
+CC=clang
+
+# 格納場所
+BUILD=./build
+LINKER=./linker
+KERNEL=./kernel
+
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# ./buildがない場合は作成
+if [ ! -e $BUILD ]; then
+    mkdir $BUILD
+fi
+
+# $LINKER/kernel.mapがない場合は作成
+if [ ! -e $LINKER/kernel.map ]; then
+    touch $LINKER/kernel.map
+fi
+
+# カーネルのビルド
+$CC $CFLAGS -Wl,-T$LINKER/kernel.ld -Wl,-Map=$LINKER/kernel.map -o $BUILD/kernel.elf $KERNEL/kernel.c
+
+# QEMUの起動
+qemu-system-riscv32 -machine virt -bios default -nographic -serial mon:stdio -kernel $BUILD/kernel.elf
